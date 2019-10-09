94fb8f81dd75436b9b5bf7159626ffe8294b0727
kpandey017 <kpandey017@gmail.com>
2019-10-09 05:53:50 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+const scrollPageToBottom = require('puppeteer-autoscroll-down')
+
+async function run() {
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+  var randomNo=Math.floor(Math.random() * (20 -10 + 1) + 10);
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1366, height: 768 })
+  await page.goto('https://www.doc2consult.com');
+  const lastPosition = await scrollPageToBottom(page);
+  await page.waitFor(randomNo*1000);
+  await page.evaluate(() => {
+        let elements = document.getElementsByTagName('article');
+		console.log(elements);
+		var item = elements[Math.floor(Math.random()*elements.length)];
+    var aElement= item.getElementsByTagName("a")[0];
+    
+    aElement.click();
+    
+    });
+
+  await page.waitFor(randomNo*1000);
+  const lastPosition = await scrollPageToBottom(page);
+  await page.waitFor(randomNo*1000);
+  browser.close();
+  
+}
+
+run();
