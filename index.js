956f3c7fad4c7050a5747e26d85c76ba40ef686c
krishna.pandey <kpandey017@gmail.com>
2020-03-14 12:54:10 +0530

    asd

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 const google = require('./google');
 const fb = require('./fb');
 const linkedin = require('./linkedin');
+const quora = require('./quora');
 
 
-var array = ['F1','F1','F2','F2','F3','F3']
+var array = ['F1','F2','F3','F4']
 
 var ranFunc;
 
 function start() { // Randomly Execute Function
  ranFunc = array[Math.floor(Math.random() * array.length)];
- if (ranFunc == 'F1') {
+  if (ranFunc == 'F1') {
     google()
  }
  if (ranFunc == 'F2') {
@@ -18,6 +19,10 @@ function start() { // Randomly Execute Function
  }
  if (ranFunc == 'F3') {
     linkedin()
+   }
+
+if (ranFunc == 'F4') {
+    quora()
    }
 
 }
