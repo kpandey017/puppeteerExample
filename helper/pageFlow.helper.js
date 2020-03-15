5867d2436d93b25e846b63752a4cc6230f1b0c2a
krishna.pandey <kpandey017@gmail.com>
2020-03-15 23:25:53 +0530

    asd

--- helper/pageFlow.helper.js
+++ helper/pageFlow.helper.js
@@ -39,34 +39,33 @@ exports.executeFlow= async function scrollDown(page1,randomNo) {
 
     //adClick   
     if(randomNo>=4 && randomNo<5){
-     
-        let allEle =[];
-        await page1.evaluate(async () => {
-        allEle =document.querySelectorAll('iFrame');
+        await page1.waitFor(4000);     
+        let cords = await page1.evaluate(() => {
+               let allAdElements = document.querySelectorAll('.advertising.advertising-footer iframe');
+              if(allAdElements.length>0){
+                  let selectedAd=allAdElements[0];
+                  let {height,width,left,right,top,x,y}=selectedAd.getBoundingClientRect();
+                  return {height, width, left, right, top, x, y};
+              }
+              else{
+                  return null;
+              }
+      
         });
-        if(allEle.length>0){
-            let clickX=0;
-            let clickY=0;
-            let elem=allEle[0];
-            const rect = await page1.evaluate(el => {
-                const { top, left, width, height } = el.getBoundingClientRect();
-                return { top, left, width, height };
-            }, elem);
-
-            clickX = rect.width / 2;
-            clickY = rect.height / 2;
-
-            clickX=clickX+rect.left;
-            clickY=clickY+rect.top;
-            await page1.mouse.click(rect.left + _x, rect.top + _y);
-
+        if(cords){
+            let calX=cords.x+ Math.round(cords.width/2)
+            let calY=cords.y+ Math.round(cords.height/2)
+          
+              await page1.mouse.click( calX, calY );      
         }
-            
+
     }
 
 
     await page1.waitFor(3000 * randomNo);
     await page1.goto('http://onemboaran.com/afu.php?zoneid=3118682');
+    await page1.waitFor(2000 * randomNo);
+    await page1.goto('https://www.passtechusa.com/hz36846v2?key=1f1bd0c663f21da0d182aa9e2a9b2bf5');
     await page1.waitFor(2000 * randomNo);
 }
 
